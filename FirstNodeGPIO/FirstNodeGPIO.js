8945c546d3631f35fc8ddb95169116c317a5d9d2
David Arnold <darnold@blackberry.com>
2015-02-24 20:25:15 +0000

    Filesystem Read

--- FirstNodeGPIO/FirstNodeGPIO.js
+++ FirstNodeGPIO/FirstNodeGPIO.js
@@ -1,3 +1,16 @@
+var fs = require('fs');
+
+console.log('Hello World');
 
 console.log('Hello World - ' + process.argv);
 
+fs.readFile('HelloWorld.txt','utf8', function(err, data) {
+	
+	if(err) 
+	{
+		return console.log("Error - " + err);
+	}
+	
+	console.log(data);
+	
+});
